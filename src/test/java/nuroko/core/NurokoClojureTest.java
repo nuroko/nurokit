4e59a1fce26685c5f3554eec64e3e8ee3029b4cb
mikera <mike.r.anderson.13@gmail.com>
2013-02-17 12:05:48 +0800

    Add Clojure tests

---
+++ src/test/java/nuroko/core/NurokoClojureTest.java
@@ -0,0 +1,11 @@
+package nuroko.core;
+
+import mikera.cljunit.ClojureTest;
+
+public class NurokoClojureTest extends ClojureTest {
+
+	@Override
+	public String filter() {
+		return "nuroko";
+	}
+}
