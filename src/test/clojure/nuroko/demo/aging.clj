7364aa472da8b59c44d3043adc023480694e7ef8
mikera <mike.r.anderson.13@gmail.com>
2013-08-16 23:28:24 +0800

    random dataset generation

--- src/test/clojure/nuroko/demo/aging.clj
+++ src/test/clojure/nuroko/demo/aging.clj
@@ -1,9 +1,25 @@
 (ns nuroko.demo.aging
   (:use [nuroko.lab core charts])
   (:use [nuroko.gui visual])
+  (:use [clojure.repl])
   (:use [clojure.core.matrix])
   (:require [task.core :as task])
   (:import [mikera.vectorz Op Ops])
   (:import [mikera.vectorz.ops ScaledLogistic Logistic Tanh])
   (:import [nuroko.coders CharCoder])
   (:import [mikera.vectorz AVector Vectorz]))
+
+(set-current-implementation :vectorz)
+
+(def ROW-LENGTH 5)
+
+;; construct a data row ( 5 elements enough?)
+(defn row ([& ds] 
+            (let [res (new-vector ROW-LENGTH)]
+              (assign! res (concat ds (repeat 0.0))))))
+
+;; training data
+(def TDATA (atom []))
+
+(dotimes [i 100]
+  (swap! TDATA (fn [old] (conj old (row (Math/sin (* i 0.1)) (Math/sin  (* i 0.06) ) (Math/sin (* i 0.05)))))))
