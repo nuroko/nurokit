dbee8fa9ea84ef752d72f67bacb0ee20bcdd7bdf
mikera <mike.r.anderson.13@gmail.com>
2013-08-17 00:40:10 +0800

    aging demo updates

--- src/test/clojure/nuroko/demo/aging.clj
+++ src/test/clojure/nuroko/demo/aging.clj
@@ -12,14 +12,49 @@
 (set-current-implementation :vectorz)
 
 (def ROW-LENGTH 5)
+(def DISPLAY-LENGTH 100)
 
 ;; construct a data row ( 5 elements enough?)
 (defn row ([& ds] 
             (let [res (new-vector ROW-LENGTH)]
               (assign! res (concat ds (repeat 0.0))))))
 
+(defn rand-row ([] (row (rand) (rand) (rand))))
+
 ;; training data
 (def TDATA (atom []))
 
-(dotimes [i 100]
-  (swap! TDATA (fn [old] (conj old (row (Math/sin (* i 0.1)) (Math/sin  (* i 0.06) ) (Math/sin (* i 0.05)))))))
+(def DATA (atom []))
+
+(defn reset []
+  (reset! TDATA [])
+  (dotimes [i 1000]
+    (swap! TDATA (fn [old] (conj old (row (Math/sin (* i 0.1)) (Math/sin  (* i 0.06) ) (Math/sin (* i 0.05))))))))
+
+(defn data-chart 
+  ([data] (data-chart data (- (count data) DISPLAY-LENGTH)))
+  ([data start] (data-chart data start (+ start DISPLAY-LENGTH)))
+  ([data start end]
+    (let [r (range (max 0 start) (min (count data) end))]
+      (xy-chart-multiline r [(map #(.get ^AVector (data %) 0) r) 
+                             (map #(.get ^AVector (data %) 1) r)
+                             (map #(.get ^AVector (data %) 2) r)]))))
+
+(defn append-data [row]
+  (swap! DATA (fn [old] (conj old row))))
+
+(reset)
+
+;; =================== DEMO CODE FOLLOWS =====================
+
+(defn demo []
+  (reset)
+  
+  (show (data-chart @TDATA 0 1000))
+  
+  (dotimes [i 100]
+     (append-data (rand-row))
+     (show (data-chart @DATA)))
+
+
+)
