258e0702e6ed730adf7be5da084ca0f76ab952dc
mikera <mike.r.anderson.13@gmail.com>
2015-08-04 15:18:29 +0800

    Nurokit test suite

---
+++ src/test/java/nuroko/core/NurokitClojureTest.java
@@ -0,0 +1,11 @@
+package nuroko.core;
+
+import mikera.cljunit.ClojureTest;
+
+public class NurokitClojureTest extends ClojureTest {
+
+	@Override
+	public String filter() {
+		return "nurokit";
+	}
+}
