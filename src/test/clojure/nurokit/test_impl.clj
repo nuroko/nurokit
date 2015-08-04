c370dfffb3ff205f0627a33a4249e380a8ca64c8
mikera <mike.r.anderson.13@gmail.com>
2015-08-04 16:01:31 +0800

    testing for MapModel lifecycle

--- src/test/clojure/nurokit/test_impl.clj
+++ src/test/clojure/nurokit/test_impl.clj
@@ -5,5 +5,9 @@
 
 (deftest test-map-model
   (let [m (imp/->MapModel {1 2 2 1})]
-    (is (thrown? Error (np/output m)))))
+    (is (thrown? Error (np/output m)))
+    (let [mt (np/think m 1)]
+      (is (= 2 (np/output mt)))
+      (let [mtc (np/clean mt)]
+        (is (thrown? Error (np/output m)))))))
 
