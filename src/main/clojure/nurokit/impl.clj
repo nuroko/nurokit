9281a7bbce844f71f35af09d665a3a80d1a6f16e
mikera <mike.r.anderson.13@gmail.com>
2015-08-19 13:10:30 +0800

    Improve input and output handling for mapmodel

--- src/main/clojure/nurokit/impl.clj
+++ src/main/clojure/nurokit/impl.clj
@@ -1,21 +1,41 @@
 (ns nurokit.impl
   "Implementation namespace for common default models"
-  (:require [nurokit.protocols :as np]))
+  (:require [nurokit.protocols :as np])
+  (:require [mikera.cljutils.error :refer [error]]))
 
 (set! *warn-on-reflection* true)
 (set! *unchecked-math* true)
 
-;; A model that implements a simple direct mapping of values
+;; A model that implements a simple direct mapping of values. Inputs can be mapped
+;; to any non-null output values
+;;
+;; may provide optional fields:
+;;  - a :create-fn that produces a default output given new input
 (defrecord MapModel [value-map]
   np/PObject
     (clean [o]
-     (dissoc o :output))
+     (-> o
+       (dissoc :output)
+       (dissoc :input)))
     (copy [o]
       (MapModel. value-map))
   np/PModel
     (think [m input]
-      (assoc m :output (value-map input)))
+      (let [recognised? (contains? value-map input)
+            m (if recognised? 
+                m 
+                (assoc-in m [:value-map input] 
+                        ((or (:create-fn m) 
+                             (error "No :create-fn defined, for input " input))
+                          input)))
+            result ((:value-map m) input)]
+        (when (nil? result) (error "No output value for input: " input))
+        (-> m
+          (assoc :output result)
+          (assoc :input input))))
     (output [m]
-      (or (:output m) (throw (Error. "Output not calculated"))))
+      (let [result (:output m)] 
+        (when (nil? result) (error "Output not calculated in MapModel" m))
+        result))
     )
 
--- src/test/clojure/nurokit/test_impl.clj
+++ src/test/clojure/nurokit/test_impl.clj
@@ -1,13 +1,19 @@
-(ns nurokit.test_impl
+(ns nurokit.test-impl
   (:use [clojure test])
   (:require [nurokit.impl :as imp])
-  (:require [nurokit.protocols :as np]))
+  (:require [nurokit.protocols :as np])
+  (:require [mikera.cljutils.error :refer [error error?]]))
 
 (deftest test-map-model
   (let [m (imp/->MapModel {1 2 2 1})]
-    (is (thrown? Error (np/output m)))
+    (is (error? (np/output m)))
     (let [mt (np/think m 1)]
+      (is (= 1 (:input mt)))
       (is (= 2 (np/output mt)))
       (let [mtc (np/clean mt)]
-        (is (thrown? Error (np/output m)))))))
+        (is (error? (np/output m)))))
+    (is (error? (np/think m 3)))
+    (let [mt (assoc m :create-fn (fn [_] :new))]
+      (is (= :new (np/output (np/think mt 3)))))))
 
+;
